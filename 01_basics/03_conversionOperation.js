df6bef0d880f0aa3201e0afaca25ddf2a787a8c4
samarth272 <samarthadkitte@gmail.com>
2024-12-14 20:00:26 +0530

    4th commit

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score=undefined
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1 ,false=>0
+
+let isLoggedIn=0
+
+let booleanisLoggedIn=Boolean(isLoggedIn)
+console.log(booleanisLoggedIn)
+
+//1=>true, 0=>false
+//""=>false
+//"hitesh"=>false
+
+let someNumber=33
+
+let stringNumber=String (someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
