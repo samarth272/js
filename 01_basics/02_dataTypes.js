c1059bb6e16a1a6e6a9d64ae522417a012e2f6ee
samarth272 <samarthadkitte@gmail.com>
2024-12-14 19:05:39 +0530

    3rd commit

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3)    //we are using nodejs, not browser
+
+console.log(3+3);
+
+console.log("Sama")
+
+let name="Samarth"
+let age=27
+let isLoggedIn=false
+
+//number is 2 to power 53
+//bigint
+//string => ""
+//boolean=> true/false
+//null=> standalone value
+//undefined=> no value assigned
+//symbol=> unique
+//object
+
+console.log(typeof undefined);   //undefined
+console.log(typeof null);        //object
