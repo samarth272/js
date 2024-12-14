a53b421bde43ccd5c05e49acc49d3949e680b8c3
samarth272 <samarthadkitte@gmail.com>
2024-12-14 17:26:40 +0530

    2nd commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId =1446576
+let accountEmail= "xyz@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState;
+
+//accountId=2
+accountEmail= "abc@gmail.com"
+accountPassword="44444"
+accountCity="Pune"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
