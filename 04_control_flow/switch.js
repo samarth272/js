a2361d7494a575e975b7195bb1e13bf7f9611488
samarth272 <samarthadkitte@gmail.com>
2024-12-19 16:04:31 +0530

    23rd commit

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,27 @@
+// switch(key){
+
+//     case value:
+//         break;
+//     default:
+//         break;
+// }
+
+const month="march"
+switch(month){
+
+    case "jan":
+        console.log("Januray");
+        break;
+    case "feb":
+        console.log("Feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+    default:
+        console.log("default case match");
+        break;
+}
